6a3fb93ad87b02463c310453f3e382d43b593a6b
WhizzIer <155801185+WhizzIer@users.noreply.github.com>
2026-03-30 12:41:10 +0200

    Game + Player + Ground
    Player can move (Basic)
    Ground for gravity testing
    next: Worldbuilding: Shapes and tilemap
    next: player animation

--- Player/player.gd
+++ Player/player.gd
@@ -3,9 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#wenn ein Knopf gedrückt wird merke das
+	
+	#wenn du das merkst, bewegung
+	#Springen auf Leertaste
+	#Gravitation
+	#move and slide
 	pass
