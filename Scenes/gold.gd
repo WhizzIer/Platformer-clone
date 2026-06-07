7f9111574756046ed3315f5219b182a7cf4e0f4e
WhizzIer <155801185+WhizzIer@users.noreply.github.com>
2026-06-07 13:20:50 +0200

    Sounds und double-jump
    Sounds:
    -Gold Pickup sound verbessert
    -jump Sound eingefügt
    
    Jump:
    -Doublejump
    -bei gold pickup +1 jump
    -reset bei Boden berühren
    
    next:
    -Status ändern durch gold Pickup bis Landung

--- Scenes/gold.gd
+++ Scenes/gold.gd
@@ -3,8 +3,8 @@ extends Area2D
 @onready var pickup_animation: AnimationPlayer = $"Pickup Animation"
 
 
+
 func _on_body_entered(body: Node2D) -> void:
-	print("Hi")
-	
 	pickup_animation.play("Pickup Animation")
-	
+	if body.has_method("collect_gold"):
+		body.collect_gold()
