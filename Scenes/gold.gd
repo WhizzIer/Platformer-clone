3664c6d80217ea87f4b00b7d117417bb4355b4a3
WhizzIer <155801185+WhizzIer@users.noreply.github.com>
2026-07-14 10:29:41 +0200

    Gold +Jump
    Gold Respawn eingebaut
    Jump verbessert (was_on_floor)

--- Scenes/gold.gd
+++ Scenes/gold.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
 @onready var pickup_animation: AnimationPlayer = $"Pickup Animation"
+@onready var respawn_timer: Timer = $RespawnTimer
+@onready var collision: CollisionShape2D = $CollisionShape2D
 
+#startet den RespawnTimer
+func respawn_timer_start() -> void:
+	visible = false
+	respawn_timer.start()
+	
 
-
+# Merkt, dass Spieler das Gold einsammelt
 func _on_body_entered(body: Node2D) -> void:
-	pickup_animation.play("Pickup Animation")
 	if body.has_method("collect_gold"):
+		pickup_animation.play("Pickup Animation")
 		body.collect_gold()
+		collision.set_deferred("disabled", true)
+
+# Respawn Timer endet
+func _on_respawn_timer_timeout() -> void:
+	visible = true
+	collision.set_deferred("disabled", false)
